8452298c763980ef70293582479d3354b410da7d
Mango2Juice <90748542+Mango2Juice@users.noreply.github.com>
2024-01-12 11:46:34 +0800

    Update install_server.sh

--- src/server_manager/install_scripts/install_server.sh
+++ src/server_manager/install_scripts/install_server.sh
@@ -407,7 +407,7 @@ function set_hostname() {
     'https://domains.google.com/checkip'
   )
   for url in "${urls[@]}"; do
-    PUBLIC_HOSTNAME="$(fetch --ipv4 "${url}")" && return
+    PUBLIC_HOSTNAME="$(fetch "${url}")" && return
   done
   echo "Failed to determine the server's IP address.  Try using --hostname <server IP>." >&2
   return 1
